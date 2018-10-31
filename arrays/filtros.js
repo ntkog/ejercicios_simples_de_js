3b3224eaf8bb9fb630f6d851c75dd52d97ca3c63
Jorge Barrachina <nusquema@gmail.com>
2018-10-31 09:29:33 +0100

    Añadido primer ejercicio

---
+++ arrays/filtros.js
@@ -0,0 +1,19 @@
+var datos = [
+  { edad : 25, sueldo : 30000, cotizados: 3  },
+  { edad : 69, sueldo : 20000, cotizados: 31 },
+  { edad : 77, sueldo : 45000, cotizados: 10 },
+  { edad : 19 , sueldo : 0, cotizados: 25 }
+];
+
+/* Ejercio 1
+Rellenar el código necesario para la siguiente función
+
+La función debe devolver aquellos items que cumplan la siguiente condiciónote
+
+ser mayor de 65 años y tener más de 25 años cotizados
+
+*/
+
+function derecho_a_pension (lista) {
+
+}
